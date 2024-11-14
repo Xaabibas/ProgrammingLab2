b6e2ed6d57d4f5f79f2edf35739c868a57f39fab
Xaabibas <tumuyhabirov@yandex.ru>
2024-11-14 14:30:00 +0300

    Вроде работает

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,32 @@
-import pokemons.*;
 import ru.ifmo.se.pokemon.Battle;
+import ru.ifmo.se.pokemon.Pokemon;
+
+import java.lang.Class;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     static final String[] teams = {"ally", "foe"};
     static final String[] pokemons = {"castform", "corphish", "crawdaunt", "poliwag", "poliwhirl", "poliwrath"};
 
-    public static boolean is_team(String team){
-        for (String i : teams){
-            if (i.equals(team.toLowerCase())){
+    public static boolean is_team(String team) {
+        for (String i : teams) {
+            if (i.equals(team.toLowerCase())) {
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean is_pokemon(String pokemon){
-        for (String i : pokemons){
-            if (i.equals(pokemon.toLowerCase())){
+    public static boolean is_pokemon(String pokemon) {
+        for (String i : pokemons) {
+            if (i.equals(pokemon.toLowerCase())) {
                 return true;
             }
         }
         return false;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
         // Данный вводим в формате Class Team Name Level
         // Class - класс покемона
@@ -35,30 +38,29 @@ public class Main {
 
         int counter = 0;
         boolean correct = true;
-        String Class = "";
+        String type = "";
         String team = "";
         String name = "";
         int level = 1;
         boolean ally = false;
         boolean foe = false;
+        Class<?>[] params = {String.class, int.class};
 
-        for (String i : args){
-            switch (counter){
+        for (String i : args) {
+            switch (counter) {
                 case 0:
-                    if (is_pokemon(i)){
-                        Class = i.toLowerCase();
+                    if (is_pokemon(i)) {
+                        type = i.substring(0, 1).toUpperCase() + i.substring(1).toLowerCase();
                         counter++;
-                    }
-                    else{
+                    } else {
                         correct = false;
                     }
                     break;
                 case 1:
-                    if (is_team(i)){
+                    if (is_team(i)) {
                         team = i.toLowerCase();
                         counter++;
-                    }
-                    else{
+                    } else {
                         correct = false;
                     }
                     break;
@@ -67,85 +69,40 @@ public class Main {
                     counter++;
                     break;
                 case 3:
-                    for (char j : i.toCharArray()){
-                        if (!Character.isDigit(j)){
+                    for (char j : i.toCharArray()) {
+                        if (!Character.isDigit(j)) {
                             correct = false;
                             break;
                         }
-                    level = Integer.parseInt(i);
+                        level = Integer.parseInt(i);
                     }
                     counter++;
             }
-            if (!correct){
+            if (!correct) {
                 System.out.println("Введен неверный формат данных!!!");
                 break;
             }
 
-            if (counter == 4){
+            if (counter == 4) {
                 counter = 0;
-                if (team.equals("ally")){
-                    ally = true;
-                }
-                else{
-                    foe = true;
-                }
-                switch (Class){
-                    case "castform":
-                         if (team.equals("ally")){
-                             b.addAlly(new Castform(name, level));
-                         }
-                         else{
-                             b.addFoe(new Castform(name, level));
-                         }
-                         break;
-                    case "corphish":
-                        if (team.equals("ally")){
-                            b.addAlly(new Corphish(name, level));
-                        }
-                        else{
-                            b.addFoe(new Corphish(name, level));
-                        }
-                        break;
-                    case "crawdaunt":
-                        if (team.equals("ally")){
-                            b.addAlly(new Crawdaunt(name, level));
-                        }
-                        else{
-                            b.addFoe(new Crawdaunt(name, level));
-                        }
-                        break;
-                    case "poliwag":
-                        if (team.equals("ally")){
-                            b.addAlly(new Poliwag(name, level));
-                        }
-                        else{
-                            b.addFoe(new Poliwag(name, level));
-                        }
-                        break;
-                    case "poliwhirl":
-                        if (team.equals("ally")){
-                            b.addAlly(new Poliwhirl(name, level));
-                        }
-                        else{
-                            b.addFoe(new Poliwhirl(name, level));
-                        }
-                        break;
-                    case "poliwrath":
-                        if (team.equals("ally")){
-                            b.addAlly(new Poliwrath(name, level));
-                        }
-                        else{
-                            b.addFoe(new Poliwrath(name, level));
-                        }
-                    }
-                }
-            }
-        if (correct){
 
-            if (ally&&foe){
-                b.go();
+                Class<?> clazz = Class.forName("pokemons." + type);
+                Pokemon pokemon = (Pokemon) clazz.getConstructor(params).newInstance(name, level);
+
+                if (team.equals("ally")) {
+                    ally = true;
+                    b.addAlly(pokemon);
+                } else {
+                    foe = true;
+                    b.addFoe(pokemon);
+                }
             }
-            else{
+        }
+
+        if (correct) {
+            if (ally && foe) {
+                b.go();
+            } else {
                 System.out.println("Одна из команд пуста!!!");
             }
         }
