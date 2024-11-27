8a02b6b476562bcf3ff03063eec5211f7a28e86b
Xaabibas <tumuyhabirov@yandex.ru>
2024-11-27 11:38:36 +0300

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import ru.ifmo.se.pokemon.Battle;
 import ru.ifmo.se.pokemon.Pokemon;
 
-import java.lang.Class;
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     static final String[] teams = {"ally", "foe"};
-    static final String[] pokemons = {"castform", "corphish", "crawdaunt", "poliwag", "poliwhirl", "poliwrath"};
 
     public static boolean is_team(String team) {
         for (String i : teams) {
@@ -17,16 +15,7 @@ public class Main {
         return false;
     }
 
-    public static boolean is_pokemon(String pokemon) {
-        for (String i : pokemons) {
-            if (i.equals(pokemon.toLowerCase())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) {
 
         // Данный вводим в формате Class Team Name Level
         // Class - класс покемона
@@ -38,35 +27,23 @@ public class Main {
 
         int counter = 0;
         boolean correct = true;
-        String type = "";
-        String team = "";
-        String name = "";
-        int level = 1;
         boolean ally = false;
         boolean foe = false;
-        Class<?>[] params = {String.class, int.class};
+        String[] data = new String[4];
 
         for (String i : args) {
             switch (counter) {
                 case 0:
-                    if (is_pokemon(i)) {
-                        type = i.substring(0, 1).toUpperCase() + i.substring(1).toLowerCase();
-                        counter++;
-                    } else {
-                        correct = false;
-                    }
+                    data[counter] = i.substring(0, 1).toUpperCase() + i.substring(1).toLowerCase();
                     break;
                 case 1:
-                    if (is_team(i)) {
-                        team = i.toLowerCase();
-                        counter++;
-                    } else {
-                        correct = false;
+                    correct = is_team(i.toLowerCase());
+                    if (correct) {
+                        data[counter] = i.toLowerCase();
                     }
                     break;
                 case 2:
-                    name = i;
-                    counter++;
+                    data[counter] = i;
                     break;
                 case 3:
                     for (char j : i.toCharArray()) {
@@ -74,31 +51,35 @@ public class Main {
                             correct = false;
                             break;
                         }
-                        level = Integer.parseInt(i);
                     }
-                    counter++;
+                    data[counter] = i;
+                    break;
             }
+            counter++;
             if (!correct) {
                 System.out.println("Введен неверный формат данных!!!");
                 break;
             }
-
             if (counter == 4) {
                 counter = 0;
 
-                Class<?> clazz = Class.forName("pokemons." + type);
-                Pokemon pokemon = (Pokemon) clazz.getConstructor(params).newInstance(name, level);
-
-                if (team.equals("ally")) {
-                    ally = true;
-                    b.addAlly(pokemon);
-                } else {
-                    foe = true;
-                    b.addFoe(pokemon);
+                try {
+                    Class<?> myClass = Class.forName("pokemons." + data[0]);
+                    Pokemon pokemon = (Pokemon) myClass.getConstructor(String.class, int.class).newInstance(data[2],
+                            Integer.parseInt(data[3]));
+                    if (data[1].equals("ally")) {
+                        ally = true;
+                        b.addAlly(pokemon);
+                    } else {
+                        foe = true;
+                        b.addFoe(pokemon);
+                    }
+                } catch (ClassNotFoundException | InvocationTargetException | InstantiationException |
+                         IllegalAccessException | NoSuchMethodException e) {
+                    throw new RuntimeException(e);
                 }
             }
         }
-
         if (correct) {
             if (ally && foe) {
                 b.go();
